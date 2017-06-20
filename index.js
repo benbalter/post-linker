c816bd9804fa9bba2dd8a37dfcef530724733776
Ben Balter <ben.balter@github.com>
2017-06-20 18:32:48 -0400

    add xo

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-module.exports = (robot) => {
+module.exports = robot => {
   robot.on('pull_request.closed', async (event, context) => {
-    var files = await context.github.pullRequests.getFiles(context.issue({}));
-    var site  = await context.github.repos.getPages(context.issue({}));
+    const files = await context.github.pullRequests.getFiles(context.issue({}));
+    const site = await context.github.repos.getPages(context.issue({}));
 
-    var comment = ""
-    for (var i in files.data)  {
-      matches = files.data[i].filename.match(/_posts\/(\d{4})-(\d{2})-(\d{2})-(.*?)\./);
-      if (!matches) { continue; }
-      var url = site.data.html_url + matches.slice(1,5).join("/") + "/";
-      var title = matches[4].replace("-", " ")
-      title = title.charAt(0).toUpperCase() + title.slice(1);
-      comment = comment + ":ship:'d! [Read the live version of '" + title + "'](" + url + ").\n";
-    }
+    let comment = '';
+    for (const file in files.data) {
+      if (files.data[file]) {
+        const matches = files.data[file].filename.match(/_posts\/(\d{4})-(\d{2})-(\d{2})-(.*?)\./);
 
-    if (comment == "") { 
-      return
-    } else { 
+        if (!matches) {
+          continue;
+        }
+
+        const url = site.data.html_url + matches.slice(1, 5).join('/') + '/';
+        let title = matches[4].replace('-', ' ');
+        title = title.charAt(0).toUpperCase() + title.slice(1);
+
+        comment = comment + ':ship:\'d! [Read the live version of \'' + title + '\'](' + url + ').\n';
+      }
+
+      if (comment === '') {
+        return;
+      }
+
       return context.github.issues.createComment(context.issue({body: comment}));
     }
   });
